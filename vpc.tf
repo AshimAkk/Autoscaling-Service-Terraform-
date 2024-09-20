f05a466b0998f96f56f51f576ea866b2edcb617f
Ashim <akkiyemba01@gmail.com>
2024-09-21 07:37:31 +1000

    created public subnet 1-3

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,36 @@ resource "aws_vpc" "webservice-vpc" {
     Name = "My-VPC"
   }
 }
+
+
+# Create Public Subnet 1
+resource "aws_subnet" "PublicSubnet1" {
+  vpc_id            = aws_vpc.webservice-vpc.id
+  cidr_block        = "192.168.0.0/24"
+  availability_zone = "ap-southeast-2a"
+  tags = {
+    Name = "PublicSubnet1"
+  }
+}
+
+
+# Create Public Subnet 2
+resource "aws_subnet" "PublicSubnet2" {
+  vpc_id            = aws_vpc.webservice-vpc.id
+  cidr_block        = "192.168.1.0/24"
+  availability_zone = "ap-southeast-2b"
+  tags = {
+    Name = "PublicSubnet2"
+  }
+}
+
+
+# Create Public Subnet 3
+resource "aws_subnet" "PublicSubnet3" {
+  vpc_id            = aws_vpc.webservice-vpc.id
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = "ap-southeast-2c"
+  tags = {
+    Name = "PublicSubnet3"
+  }
+}
